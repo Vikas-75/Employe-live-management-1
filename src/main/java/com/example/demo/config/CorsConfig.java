c4cde82d7e8efe1fdf0e736e0412e0f959a198d0
Vikas-75 <vm1705654@gmail.com>
2026-01-03 18:06:49 +0530

    Fix allowedOrigins URL in CorsConfig

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://auth-frontend4.vercel.app/")
+                .allowedOrigins("https://auth-frontend4.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
