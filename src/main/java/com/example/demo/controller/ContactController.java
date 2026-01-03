14c05924f37108cdef228800e01ccde80c304c5e
Vikas-75 <vm1705654@gmail.com>
2026-01-03 18:59:24 +0530

    Update ContactController.java

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/contact")
+@RequestMapping("/contact")
 @CrossOrigin(origins = "*")
 public class ContactController {
 
