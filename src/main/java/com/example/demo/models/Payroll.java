016a3b8d027b1096691e2f5a11aee41bdf772352
Vikas-75 <vm1705654@gmail.com>
2026-01-07 12:02:53 +0530

    Create Payroll.java

---
+++ src/main/java/com/example/demo/models/Payroll.java
@@ -0,0 +1,122 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+  uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"employee_id", "month", "year"})
+  }
+)
+public class Payroll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    private double baseSalary;
+    private double bonus;
+    private double totalDeductions;
+    private double grossSalary;
+    private double netSalary;
+
+    private int month;
+    private int year;
+
+    private LocalDateTime generatedDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public double getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+
+	public double getTotalDeductions() {
+		return totalDeductions;
+	}
+
+	public void setTotalDeductions(double totalDeductions) {
+		this.totalDeductions = totalDeductions;
+	}
+
+	public double getGrossSalary() {
+		return grossSalary;
+	}
+
+	public void setGrossSalary(double grossSalary) {
+		this.grossSalary = grossSalary;
+	}
+
+	public double getNetSalary() {
+		return netSalary;
+	}
+
+	public void setNetSalary(double netSalary) {
+		this.netSalary = netSalary;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public LocalDateTime getGeneratedDate() {
+		return generatedDate;
+	}
+
+	public void setGeneratedDate(LocalDateTime localDateTime) {
+		this.generatedDate = localDateTime;
+	}
+   	
+}
